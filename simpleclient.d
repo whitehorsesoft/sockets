d4b5fa8b5bddc8af7a85ecc6edd918830d00e59f
devops <devops@whitehorsesoft.com>
2018-05-07 20:46:09 -0500

    added some simple two-way communication

--- simpleclient.d
+++ simpleclient.d
@@ -1,24 +1,37 @@
 import std.socket;
 import std.stdio;
+import std.format;
+import std.concurrency;
 
 void main()
 {
-    auto localadd = getAddress("127.0.0.1", 4444)[0];
-    writefln("Client on port 4444");
+    auto localadd = getAddress("127.0.0.1", 50101)[0];
+    writefln("Client on port 50101");
     foreach(i; 0..10) {
       auto skt = new TcpSocket(localadd);
       writeln("connected");
 
-      char[] line;
-      char[1] buf;
-      while(skt.receive(buf)) {
-        if (buf[0] == '\n') break;
-        line ~= buf;
-      }
+      auto line = skt.getLine;
       writeln(line);
-      while (true) {
+      if (line == "start") {
+        spawn(&worker);
       }
+      skt.send(format("test response %s \n", i));
       skt.shutdown(SocketShutdown.BOTH);
       skt.close;
     }
+}
+
+string getLine(TcpSocket skt) {
+  char[] line;
+  char[1] buf;
+  while(skt.receive(buf)) {
+    if (buf[0] == '\n') break;
+    line ~= buf;
+  }
+  return line.dup;
+}
+
+void worker() {
+  writeln("worker started");
 }
--- simpleserver.d
+++ simpleserver.d
@@ -1,5 +1,6 @@
 import std.socket;
 import std.stdio;
+import std.string;
 
 void main()
 {
@@ -8,12 +9,23 @@ void main()
       skt.shutdown(SocketShutdown.BOTH);
       skt.close;
     }
-    skt.bind(new InternetAddress(4444));
+    skt.bind(new InternetAddress(50101));
     skt.listen(1); // 1 pending incoming connections are queued
-    writefln("Server on port 4444");
+    writefln("Server on port 50101");
     while (true) {
-      auto sn2 = skt.accept();
+      auto sn2 = skt.accept;
       writeln("connected");
-      sn2.send("hello\n");
+      writeln("what would you like to send?");
+      string sendText;
+      sendText = readln.strip;
+      sn2.send(sendText ~ "\n");
+
+      char[] line;
+      char[1] buf;
+      while(sn2.receive(buf)) {
+        if (buf[0] == '\n') break;
+        line ~= buf;
+      }
+      writeln("recd: " ~ line);
     }
 }
